fix: get somewhat stable gradients

module.py
@@ -6,40 +6,65 @@ def mish(fn_input: torch.Tensor) -> torch.Tensor:
     return fn_input * torch.tanh(torch.nn.functional.softplus(fn_input))
 
 
+# class Binary(torch.autograd.Function):
+#     @staticmethod
+#     def forward(ctx, fn_input):
+#         if fn_input.requires_grad:
+#             ctx.save_for_backward(fn_input)
+#         else:
+#             ctx.no_grad = True
+#         return fn_input.sign()
+#
+#     @staticmethod
+#     def backward(ctx, grad_output):
+#         if hasattr(ctx, 'no_grad'):
+#             return None
+#         fn_input, = ctx.saved_tensors
+#         return grad_output * fn_input.sign()
+
+@torch.jit.script
+def divact(x: torch.Tensor) -> torch.Tensor:
+    return torch.nn.functional.sigmoid(x).add(1)
+
+
+binarize = divact  # lambda x: x.tanh().div(2)
+
+
 class ReversibleRNNFunction(torch.autograd.Function):
     @staticmethod
-    def _calc(fn_input, sequence_input, bn_weight, bn_bias, linear_param):
-        std, mean = torch.std_mean(fn_input, 0, keepdim=True)
-        out = (fn_input - mean) / (std + 1e-6) * bn_weight + bn_bias
-        out = mish(out)
-        out0, out1 = torch.nn.functional.linear(torch.cat([out, sequence_input], 1), linear_param, None).chunk(2, 1)
-        out1 = out1.sigmoid()
-        return out0 * out1, 1 - out1
+    def _calc(fn_input, sequence_input, linear_param):
+        # std, mean = torch.std_mean(fn_input, 0, keepdim=True)
+        # out = (fn_input - mean) / (std + 1e-5)
+        out = fn_input
+        out0, out1, out2 = torch.nn.functional.linear(torch.cat([out, sequence_input], 1),
+                                                      linear_param,
+                                                      None).chunk(3, 1)
+        weight = torch.nn.functional.relu6(out1)
+        out0 = out0 * weight
+        print("C", out0.mean().item(), weight.mean().item())
+        return out0, weight + 1
 
     @staticmethod
-    def _forward_pass(fn_input, sequence_input, linear_param0, linear_param1, bn_weight0, bn_bias0, bn_weight1,
-                      bn_bias1):
+    def _forward_pass(fn_input, sequence_input, linear_param0, linear_param1):
         inp = fn_input.chunk(2, 1)
-        print(inp[0].mean().item(), inp[1].mean().item(), "F")
-
         out = inp[0]
-        params = [[bn_weight0, bn_bias0, linear_param0], [bn_weight1, bn_bias1, linear_param1]]
+        params = [linear_param0, linear_param1]
         outputs = [None, None]
         for i in range(2):
-            tmp0, tmp1 = ReversibleRNNFunction._calc(out, sequence_input, *params[i])
-            outputs[1 - i] = out = tmp0 + inp[1 - i] * tmp1
+            tmp0, tmp1 = ReversibleRNNFunction._calc(out, sequence_input, params[i])
+            outputs[1 - i] = out = (inp[1 - i] + tmp0) / tmp1
         out = torch.cat(outputs, 1)
+        print("FP", out.mean().item())
         return out
 
     @staticmethod
-    def _backward_one(out, inp, sequence_input, bn_weight, bn_bias, linear_param):
-        tmp0, tmp1 = ReversibleRNNFunction._calc(inp, sequence_input, bn_weight, bn_bias, linear_param)
-        return (out - tmp0) / tmp1
+    def _backward_one(out, inp, sequence_input, linear_param):
+        tmp0, tmp1 = ReversibleRNNFunction._calc(inp, sequence_input, linear_param)
+        return out * tmp1 - tmp0
 
     @staticmethod
-    def forward(ctx, fn_input, _sequence_input, linear_param0, linear_param1, bn_weight0, bn_bias0, bn_weight1,
-                bn_bias1, top, pos_enc):
-        output_list = []
+    def forward(ctx, fn_input, _sequence_input, linear_param0, linear_param1, output_listoutput_list, top, pos_enc):
+        ctx.save_for_backward(_sequence_input, linear_param0, linear_param1, pos_enc)
         ctx.output_list = output_list
         ctx.top = top
 
@@ -47,46 +72,42 @@ class ReversibleRNNFunction(torch.autograd.Function):
         if output_list:
             output_list.clear()
         with torch.no_grad():
-            print("FORWARD")
-            out = ReversibleRNNFunction._forward_pass(fn_input, sequence_input, linear_param0, linear_param1,
-                                                      bn_weight0, bn_bias0, bn_weight1, bn_bias1)
+            out = ReversibleRNNFunction._forward_pass(fn_input, sequence_input, linear_param0, linear_param1)
         with torch.enable_grad():
             out.requires_grad_(True)
-            ctx.save_for_backward(_sequence_input, linear_param0, linear_param1, bn_weight0, bn_bias0, bn_weight1,
-                                  bn_bias1,
-                                  pos_enc, out)
+            output_list.append(out)
+            print("F", out.mean().item())
             return out
 
     @staticmethod
     def backward(ctx, grad_output):
-        print("BACK")
-        sequence_input, linear_param0, linear_param1, bn_weight0, bn_bias0, bn_weight1, bn_bias1, pos_enc, out = ctx.saved_tensors
+        sequence_input, linear_param0, linear_param1, pos_enc = ctx.saved_tensors
         _sequence_input = torch.cat([sequence_input, pos_enc], -1)
         _sequence_input.requires_grad_(sequence_input.requires_grad)
         sequence_input = _sequence_input
         if not sequence_input.requires_grad:
-            print("NO GRAD")
-            return (None,) * 10
-        out0, out1 = out.chunk(2, 1)
+            return (None,) * 7
+        out0, out1 = ctx.output_list.pop(0).chunk(2, 1)
         with torch.no_grad():
-            inp0 = ReversibleRNNFunction._backward_one(out0, out1, sequence_input, bn_weight1, bn_bias1, linear_param1)
-            inp1 = ReversibleRNNFunction._backward_one(out1, inp0, sequence_input, bn_weight0, bn_bias0, linear_param0)
-        print(inp0.mean().item(), inp1.mean().item(), "B")
+            inp0 = ReversibleRNNFunction._backward_one(out0, out1, sequence_input, linear_param1)
+            inp1 = ReversibleRNNFunction._backward_one(out1, inp0, sequence_input, linear_param0)
         with torch.enable_grad():
             fn_input = torch.cat([inp0, inp1], 1)
             fn_input.detach_()
             fn_input.requires_grad_(True)
-            args = (fn_input, sequence_input, linear_param0, linear_param1, bn_weight0, bn_bias0, bn_weight1, bn_bias1)
+            print("B", fn_input.mean().item())
+            args = (fn_input, sequence_input, linear_param0, linear_param1)
             grad_out = ReversibleRNNFunction._forward_pass(*args)
         grad_out.requires_grad_(True)
-        grad_out = list(torch.autograd.grad(grad_out, args, grad_output, allow_unused=True))
+        grad_out = list(
+            torch.autograd.grad(grad_out, (fn_input, sequence_input, linear_param0, linear_param1), grad_output,
+                                allow_unused=True))
         grad_out[1] = grad_out[1][:, :-2]
         fn_input.detach_()
         fn_input.requires_grad_(True)
         if not ctx.top:
             ctx.output_list.append(fn_input)
-        print("OUT")
-        return tuple(grad_out) + (None, None)
+        return tuple(grad_out) + (None,) * 3
 
 
 class RevRNN(torch.nn.Module):
@@ -106,28 +127,16 @@ class RevRNN(torch.nn.Module):
         torch.nn.init.normal_(self.hidden_state)
 
         def set_param(self, idx):
-            linear_param0 = torch.nn.Parameter(torch.zeros((2 * hidden_features, input_features + hidden_features)))
-            linear_param1 = torch.nn.Parameter(torch.zeros((2 * hidden_features, input_features + hidden_features)))
-            bn_weight0 = torch.nn.Parameter(torch.ones(hidden_features))
-            bn_weight1 = torch.nn.Parameter(torch.ones(hidden_features))
-            bn_bias0 = torch.nn.Parameter(torch.zeros(hidden_features))
-            bn_bias1 = torch.nn.Parameter(torch.zeros(hidden_features))
+            linear_param0 = torch.nn.Parameter(torch.zeros((3 * hidden_features, input_features + hidden_features)))
+            linear_param1 = torch.nn.Parameter(torch.zeros((3 * hidden_features, input_features + hidden_features)))
 
             torch.nn.init.orthogonal_(linear_param0)
-            torch.nn.init.orthogonal_(linear_param1)
-            torch.nn.init.uniform_(bn_weight0, 0.998, 1.002)
-            torch.nn.init.uniform_(bn_weight1, 0.998, 1.002)
-            torch.nn.init.uniform_(bn_bias0, -0.002, 0.002)
-            torch.nn.init.uniform_(bn_bias1, -0.002, 0.002)
+            torch.nn.init.orthogonal_(linear_param0)
 
             setattr(self, f'linear_param0{idx}', linear_param0)
             setattr(self, f'linear_param1{idx}', linear_param1)
-            setattr(self, f'bn_weight0{idx}', bn_weight0)
-            setattr(self, f'bn_weight1{idx}', bn_weight1)
-            setattr(self, f'bn_bias0{idx}', bn_bias0)
-            setattr(self, f'bn_bias1{idx}', bn_bias1)
 
-            return [linear_param0, linear_param1, bn_weight0, bn_bias0, bn_weight1, bn_bias1]
+            return linear_param0, linear_param1
 
         self.parameter_list = [set_param(self, i) for i in range(depth)]
 
@@ -150,13 +159,15 @@ class RevRNN(torch.nn.Module):
         seq += self.delay
         factor = (seq + 1) / 2
         indices = torch.arange(1, seq + 1, dtype=torch.float, device=fn_input.device).view(1, -1, 1)
-        positional_encoding = torch.cat([indices, (indices - factor) / factor], -1).expand(batch, -1, -1)
+        positional_encoding = torch.cat([indices, (indices - factor) / factor], -1).expand(batch, -1, -1).to(
+            fn_input.dtype)
         for idx in range(base_seq):
             out = self._apply_forward(fn_input[:, idx], out, output_list, output, top, positional_encoding[:, idx])
             top = False
         for idx in range(base_seq, seq):
-            out = self._apply_forward(torch.zeros(batch, input_features, device=fn_input.device),
-                                      out, output_list, output, top, positional_encoding[:, idx])
+            out = self._apply_forward(
+                torch.zeros((batch, input_features), device=fn_input.device, dtype=fn_input.dtype),
+                out, output_list, output, top, positional_encoding[:, idx])
             top = False
         if self.return_sequences:
             out = torch.stack(output[self.delay:], 1)

train.py
@@ -5,9 +5,9 @@ import torch_optimizer
 
 import module
 
-HIDDEN = 1024
-DELAY = 8
-BATCH_SIZE = 256
+HIDDEN = 256
+DELAY = 4
+BATCH_SIZE = 64
 SEQUENCE_LENGTH = 256
 DROPOUT_RATE = 0.15
 PRINTERVALL = 64
@@ -27,10 +27,8 @@ def init(module: torch.nn.Module):
 
 
 mod = torch.nn.Sequential(torch.nn.Embedding(256, 256),
-                          module.RevRNN(256, HIDDEN, delay=DELAY, return_sequences=True, depth=DEPTH),
-                          module.Transpose(),
-                          torch.nn.BatchNorm1d(HIDDEN),
-                          torch.nn.Conv1d(HIDDEN, 256, 1)).to(DEVICE)
+                          module.RevRNN(256, HIDDEN, delay=DELAY, return_sequences=True, depth=DEPTH)).to(
+    DEVICE).double()
 mod.apply(init)
 opt = torch_optimizer.Yogi(mod.parameters(), lr=1e-3, weight_decay=1e-2)
 
@@ -53,8 +51,10 @@ while True:
     start_time = time.time()
     for i in range(1, 1 + length):
         tgt = tensor[batch_index].to(DEVICE)
-        src = tgt * tgt.bernoulli(p=1-DROPOUT_RATE)
+        src = tgt * tgt.bernoulli(p=1 - DROPOUT_RATE)
         out = mod(src.to(DEVICE))
+        out.transpose_(1, 2)
+        out = out[:, :256]
         lss = torch.nn.functional.cross_entropy(out, tgt)
         lss.backward()
         opt.step()