fix(model): do backprop

module.py
@@ -1,11 +1,13 @@
 import typing
 
 import torch
+import torch.nn.functional
 
 
 @torch.jit.script
 def _activate_norm(fn_input: torch.Tensor) -> torch.Tensor:
-    return torch.nn.functional.relu(fn_input)
+    return torch.nn.functional.instance_norm(torch.nn.functional.relu(fn_input).unsqueeze(1)).squeeze(1)
+
 
 
 @torch.jit.script
@@ -18,7 +20,7 @@ def _calc(fn_input: torch.Tensor, sequence_input: torch.Tensor, linear_param_a: 
     fn_input = _activate_norm(fn_input)
     b = torch.mm(fn_input, linear_param_a)
     c = torch.mm(sequence_input, linear_param_b)
-    o = _activate_norm(b * c)
+    o = _activate_norm(b + c)
     o = torch.mm(o, linear_param_c)
     o = o.reshape(batch, features_sqrt, features_sqrt)
     return o.qr().Q
@@ -60,64 +62,54 @@ def _fn_forward(inp0: torch.Tensor, inp1: torch.Tensor, sequence_input: torch.Te
     return _enable_grad(out)
 
 
-@torch.jit.script
-def _fn_backward(out0: torch.Tensor, out1: torch.Tensor, sequence_input: torch.Tensor, linear_param0a: torch.Tensor,
-                 linear_param0b: torch.Tensor, linear_param0c: torch.Tensor, linear_param1a: torch.Tensor,
-                 linear_param1b: torch.Tensor, linear_param1c: torch.Tensor,
-                 grad_output: typing.Tuple[torch.Tensor, torch.Tensor]
-                 ):
-    with torch.no_grad():
-        inp0 = _backward_one(out0, out1, sequence_input, linear_param1a, linear_param1b, linear_param1c)
-        inp1 = _backward_one(out1, inp0, sequence_input, linear_param0a, linear_param0b, linear_param0c)
-    torch.is_grad_enabled()
-    inp0.detach_()
-    inp1.detach_()
-    fn_input = _enable_grad((inp0, inp1))
-    args = fn_input + (sequence_input, linear_param0a, linear_param0b, linear_param0c, linear_param1a,
-                       linear_param1b, linear_param1c)
-    grad_out = _enable_grad(_forward_pass(*args))
-    grad_out = torch.autograd.grad(grad_out, args, grad_output, allow_unused=True)
-    inp0.detach_()
-    inp1.detach_()
-    return grad_out, _enable_grad((inp0, inp1))
-
-
 class ReversibleRNNFunction(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, inp0: torch.Tensor, inp1: torch.Tensor, sequence_input: torch.Tensor, linear_param0a: torch.Tensor,
-                linear_param0b: torch.Tensor, linear_param0c: torch.Tensor, linear_param1a: torch.Tensor,
-                linear_param1b: torch.Tensor, linear_param1c: torch.Tensor,
-                output_list: typing.List[typing.Tuple[torch.Tensor, torch.Tensor]],
-                top: bool, embedding: torch.Tensor):
+    def forward(ctx, inp0: torch.Tensor, inp1: torch.Tensor,
+                linear_param0a: torch.Tensor, linear_param0b: torch.Tensor, linear_param0c: torch.Tensor,
+                linear_param1a: torch.Tensor, linear_param1b: torch.Tensor, linear_param1c: torch.Tensor,
+                embedding: torch.Tensor, tmp_inp0: torch.Tensor, tmp_inp1: torch.Tensor, sequence_input: torch.Tensor,
+                bottom: bool):
         ctx.save_for_backward(sequence_input, linear_param0a, linear_param0b, linear_param0c, linear_param1a,
                               linear_param1b, linear_param1c, embedding)
+        with torch.no_grad():
+            sequence_input = embedding[sequence_input].detach()
+
+            out = _fn_forward(inp0, inp1, sequence_input, linear_param0a, linear_param0b, linear_param0c,
+                              linear_param1a,
+                              linear_param1b, linear_param1c)
+            return out + (inp0, inp1)
+
+    @staticmethod
+    def backward(ctx, grad0: torch.Tensor, grad1: torch.Tensor, out0: torch.Tensor, out1: torch.Tensor):
+        sequence_input, l0a, l0b, l0c, l1a, l1b, l1c, embedding = ctx.saved_tensors
         sequence_input = embedding[sequence_input]
-        ctx.output_list = output_list
-        ctx.top = top
+        with torch.no_grad():
+            inp0 = _backward_one(out0, out1, sequence_input, l1a, l1b, l1c)
+            inp1 = _backward_one(out1, inp0, sequence_input, l0a, l0b, l0c)
+        with torch.enable_grad():
+            inp0.detach_()
+            inp1.detach_()
+            fn_input = _enable_grad((inp0, inp1))
+            args = (sequence_input, l0a, l0b, l0c, l1a, l1b, l1c)
+            grad_out = _enable_grad(_forward_pass(*fn_input, *args))
+            grad_out = torch.autograd.grad(grad_out, fn_input + args[1:] + (embedding,), (grad0, grad1),
+                                           allow_unused=True)
+        return grad_out + fn_input + (None, None,)
 
-        if output_list:
-            output_list.clear()
 
-        out = _fn_forward(inp0, inp1, sequence_input, linear_param0a, linear_param0b, linear_param0c, linear_param1a,
-                          linear_param1b, linear_param1c)
-        output_list.append(out)
-        return out
+class ReplaceGrad(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, inp0: torch.Tensor, inp1: torch.Tensor, tmp_inp0: torch.Tensor, tmp_inp1: torch.Tensor):
+        ctx.save_for_backward(tmp_inp0, tmp_inp1)
+        return inp0, inp1
 
     @staticmethod
     def backward(ctx, grad0: torch.Tensor, grad1: torch.Tensor):
-        sequence_input, l0a, l0b, l0c, l1a, l1b, l1c, embedding = ctx.saved_tensors
-        sequence_input = embedding[sequence_input]
-        if not sequence_input.requires_grad:
-            return (None,) * 8
-
-        out = ctx.output_list.pop(0)
-        grad_out, fn_input = _fn_backward(out, sequence_input, l0a, l0b, l0c, l1a, l1b, l1c, (grad0, grad1))
-
-        if not ctx.top:
-            ctx.output_list.append(fn_input)
-        return grad_out + (None,) * 3
+        tmp_inp0, tmp_inp1 = ctx.saved_tensors
+        return grad0, grad1, tmp_inp0, tmp_inp1
 
 
+replace = ReplaceGrad().apply
 fn = ReversibleRNNFunction().apply
 
 
@@ -157,30 +149,32 @@ class FixedRevRNN(torch.nn.Module):
         self.register_buffer("hidden_state0", torch.randn(features_sqrt, features_sqrt).qr().Q.unsqueeze(0))
         self.register_buffer("hidden_state1", torch.randn(features_sqrt, features_sqrt).qr().Q.unsqueeze(0))
 
-    def _call(self, out0: torch.Tensor, out1: torch.Tensor, sequence_input: torch.Tensor, top: bool,
-              output: typing.List[typing.Tuple[torch.Tensor, torch.Tensor]]
-              ) -> typing.Tuple[torch.Tensor, torch.Tensor]:
-        out = fn(out0, out1, sequence_input, self.linear_param0a, self.linear_param0b, self.linear_param0c,
-                 self.linear_param1a, self.linear_param1b, self.linear_param1c, output, top, self.embedding)
-        output.append(out)
-        return out
+    def _call(self, inp: typing.Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor],
+              sequence_inp: torch.Tensor,
+              bottom: bool) -> typing.Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        return fn(inp[0], inp[1], self.linear_param0a, self.linear_param0b, self.linear_param0c, self.linear_param1a,
+                  self.linear_param1b, self.linear_param1c, self.embedding, inp[2], inp[3], sequence_inp, bottom)
 
     def forward(self, fn_input: torch.Tensor):
         # B, S -> B, S, H, H -> B, S, F
-        output_list = []
         batch = fn_input.size(0)
         out = (self.hidden_state0.expand(batch, -1, -1).requires_grad_(True),
-               self.hidden_state0.expand(batch, -1, -1).requires_grad_(True))
+               self.hidden_state1.expand(batch, -1, -1).requires_grad_(True)) * 2
         base_seq = seq = self.input_count
         seq += self.delay
+        outputs: typing.List[typing.Optional[torch.Tensor]] = []
         zeros = torch.zeros(1, device=fn_input.device, dtype=fn_input.dtype).expand(batch)
         for idx in range(base_seq):
-            out = self._call(out[0], out[1], fn_input[:, idx], not idx, output_list)
+            out = self._call(out, fn_input[:, idx], not idx)
+            if idx >= self.delay:
+                outputs.extend(list(out)[:2])
         for idx in range(base_seq, seq):
-            out = self._call(out[0], out[1], zeros, not idx, output_list)
-        out = torch.stack(output_list[self.delay:], 1).view(batch, base_seq, -1)
-        out = torch.bmm(out, self.out_linear.expand(batch, -1, -1))
-        return out
+            out = self._call(out, zeros, not idx)
+            outputs.extend(list(out)[:2])
+        out = replace(*out)
+        outputs = outputs[:-2] + list(out)
+        out = torch.cat(outputs[self.delay:], 1).view(batch, base_seq, -1)
+        return torch.bmm(out, self.out_linear.expand(batch, -1, -1))
 
 
 class Transpose(torch.nn.Module):

train.py
@@ -25,12 +25,11 @@ torch._C._set_cublas_allow_tf32(True)
 torch._C._jit_set_inline_everything_mode(True)
 torch._C._jit_set_texpr_fuser_enabled(True)
 
-HIDDEN = 64  # hidden units are squared
+HIDDEN = 16  # hidden units are squared
 DELAY = 0
-BATCH_SIZE = 2
-SEQUENCE_LENGTH = 2 ** 14
+BATCH_SIZE = 256
+SEQUENCE_LENGTH = 2**5
 PRINTERVALL = 1
-DEPTH = 1
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 DTYPE = torch.float  # torch.double
 
@@ -74,7 +73,7 @@ base_index = batch_index.clone()
 length = tensor.size(0) // SEQUENCE_LENGTH - 1
 len_len = len(str(length))
 
-mod = torch.jit.trace(mod, tensor[batch_index].to(DEVICE))
+# mod = torch.jit.trace(mod, tensor[batch_index].to(DEVICE))
 opt = torch.optim.AdamW(mod.parameters(), lr=0.0625 * 0.5, weight_decay=2e-4)
 sch = torch.optim.lr_scheduler.ReduceLROnPlateau(opt, patience=256, factor=0.4)  # 1024
 
@@ -101,9 +100,11 @@ while True:
                 acc = (tgt == out.argmax(1)).sum().detach() / tgt.numel() * 100
                 mean_acc += acc
                 print(
-                    f"[{i:{len_len}d}/{length}] Loss: {curr_loss.item() / PRINTERVALL:7.4f} - Mean: {mean_loss.item() / i:7.4f}"
-                    f" | Acc: {acc.item():6.2f}% - Mean: {mean_acc.item() / (i / PRINTERVALL):6.2f}% - LR: {opt.param_groups[0]['lr']:.6f}"
-                    f" | Batch/s: {i / (time.time() - start_time):.3f}s")
+                    f"[{i:{len_len}d}/{length}] Loss: {curr_loss.item() / PRINTERVALL:7.4f} - "
+                    f"Mean: {mean_loss.item() / i:7.4f}"
+                    f" | Acc: {acc.item():6.2f}% - Mean: {mean_acc.item() / (i / PRINTERVALL):6.2f}% - "
+                    f"LR: {opt.param_groups[0]['lr']:.6f}"
+                    f" | Batch/s: {i / (time.time() - start_time):.3f}")
                 curr_loss = 0
                 sch.step(curr_loss)
 