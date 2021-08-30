6b456ca51d6a5500d50b35e440cc8662d57bb5ac
ClashLuke <39779310+clashluke@users.noreply.github.com>
2021-08-30 08:31:35 +0200

    feat(preprocessing): add ftfy

--- scripts/preprocess_dataset.py
+++ scripts/preprocess_dataset.py
@@ -1,7 +1,9 @@
+import ftfy
 import numpy as np
 import torch
 
-with open("data.txt", 'rb') as f:
+with open("data.txt", 'r', errors="ignore") as f:
     dat = f.read()
+dat = ftfy.fix_text(dat).encode('UTF-8')
 tensor = torch.as_tensor(np.frombuffer(dat, np.uint8))
 torch.save(tensor, '../out.tensor')
