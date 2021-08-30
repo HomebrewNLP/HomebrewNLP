5657cfc3fa9027cd2b64395f82c8e49c79c5af77
ClashLuke <39779310+clashluke@users.noreply.github.com>
2021-08-30 08:40:24 +0200

    style(train): extend rate printout

--- src/train.py
+++ src/train.py
@@ -28,8 +28,11 @@ def main(ctx: Context):
                 curr_loss += lss.detach()
                 if i % ctx.log.loss_steps_per_print == 0:
                     mean_loss += curr_loss
-                    print(f"[{i:{len_len}d}/{length}] Loss: {curr_loss.item() / ctx.log.loss_steps_per_print:7.4f} -",
+                    rate = i / (time.time() - start_time)
+                    print(f"[{i:{len_len}d}/{length}]",
+                          "Loss: {curr_loss.item() / ctx.log.loss_steps_per_print:7.4f} -",
                           f"Mean: {mean_loss.item() / i:7.4f} |",
-                          f"LR: {opt.param_groups[0]['lr']:.6f}",
-                          f"| Batch/s: {i / (time.time() - start_time):.3f}")
+                          f"LR: {opt.param_groups[0]['lr']:.6f} |",
+                          f"Batch/s: {rate:6.3f} -",
+                          f"Tokens/day: {3600 * 24 * rate * ctx.model.batch_size * ctx.model.sequence_length:11,.0f}")
                     curr_loss = 0
