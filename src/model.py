1f47acd99e5a828657363689d4986044701d0f99
ClashLuke <39779310+clashluke@users.noreply.github.com>
2021-08-30 06:57:20 +0200

    fix(model): use correct shapes in dropout

--- src/model.py
+++ src/model.py
@@ -24,22 +24,25 @@ def conv(inp: torch.Tensor, weight: torch.Tensor) -> torch.Tensor:
 def drop_conv(inp: torch.Tensor, weight: torch.Tensor, p: float,
               train: bool) -> torch.Tensor:
     batch, features, sequence = inp.size()
-    tmp = torch.relu(inp)
     if 0 < p < 1:
         if train:
-            mask = torch.randn((features,), device=tmp.device, dtype=tmp.dtype) < p
+            mask = torch.randn((features,), device=inp.device, dtype=inp.dtype) < p
             inp = torch.masked_select(inp, mask.view(1, -1, 1)).view(batch, -1, sequence)
-            weight = torch.masked_select(weight, mask.view(-1, 1, 1, 1)).view(-1, weight.size(1), weight.size(2), weight.size(3))
-            return conv(inp, weight)
-        return conv(inp, weight) * p
+            weight = torch.masked_select(weight, mask.view(-1, 1, 1)).view(-1, weight.size(1), weight.size(2))
+        elif torch.numel(inp) > torch.numel(weight):
+            weight = weight * p
+        else:
+            inp = inp * p
     return conv(inp, weight)
 
 
 @torch.jit.script
 def feed_forward(inp: torch.Tensor, w0: torch.Tensor, w1: torch.Tensor, w2: torch.Tensor, p: float,
                  train: bool) -> torch.Tensor:
-    inp = conv(inp, w0)
+    inp = drop_conv(inp, w0, p, train)
+    inp = torch.relu(inp)
     inp = drop_conv(inp, w1, p, train)
+    inp = torch.relu(inp)
     inp = drop_conv(inp, w2, p, train)
     return inp
 
