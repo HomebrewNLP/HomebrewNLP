fb58f5e272e6db3eda15ede3b2263bab306c4722
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-09-01 20:31:46 +0200

    style(train): remove unecessary while true

--- src/train.py
+++ src/train.py
@@ -20,24 +20,24 @@ def main(ctx: Context):
     dtype = torch.float16 if ctx.model.float16 else torch.float
     mean_loss = torch.zeros([], device=ctx.model.device, dtype=dtype)
     curr_loss = torch.zeros([], device=ctx.model.device, dtype=dtype)
-
-    while True:
-        start_time = time.time()
-        for i, (src, tgt) in enumerate(data, 1):
-            lss = mod(src.squeeze(0).to(device=ctx.model.device, non_blocking=True),
-                      tgt.squeeze(0).to(device=ctx.model.device, non_blocking=True))
-            mod.backward(lss)
-            with torch.no_grad():
-                mod.step()
-                lr_scheduler.step()
-                curr_loss += lss.detach()
-                if i % ctx.log.loss_steps_per_print == 0:
-                    mean_loss += curr_loss
-                    rate = i / (time.time() - start_time)
-                    print(f"[{i:{len_len}d}/{length}]",
-                          f"Loss: {curr_loss.item() / ctx.log.loss_steps_per_print:7.4f} -",
-                          f"Mean: {mean_loss.item() / i:7.4f} |",
-                          f"LR: {opt.param_groups[0]['lr']:.6f} |",
-                          f"Batch/s: {rate:6.3f} -",
-                          f"Tokens/day: {3600 * 24 * rate * ctx.model.batch_size * ctx.model.sequence_length:11,.0f}")
-                    curr_loss = 0
+    start_time = time.time()
+    for i, (src, tgt) in enumerate(data, 1):
+        lss = mod(src.squeeze(0).to(device=ctx.model.device, non_blocking=True),
+                  tgt.squeeze(0).to(device=ctx.model.device, non_blocking=True))
+        mod.backward(lss)
+        with torch.no_grad():
+            mod.step()
+            lr_scheduler.step()
+            curr_loss += lss.detach()
+            if i % ctx.log.loss_steps_per_print == 0:
+                mean_loss += curr_loss
+                rate = i / (time.time() - start_time)
+                print(f"[{i:{len_len}d}/{length}]",
+                      f"Loss: {curr_loss.item() / ctx.log.loss_steps_per_print:7.4f} -",
+                      f"Mean: {mean_loss.item() / i:7.4f} |",
+                      f"LR: {opt.param_groups[0]['lr']:.6f} |",
+                      f"Batch/s: {rate:6.3f} -",
+                      f"Tokens/day: {3600 * 24 * rate * ctx.model.batch_size * ctx.model.sequence_length:11,.0f}")
+                curr_loss = 0
+        if steps is not None and i > steps:
+            return
