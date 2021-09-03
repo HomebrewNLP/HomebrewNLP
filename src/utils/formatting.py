84cb4c63b4ed8281a040d42fadac527d3c96f81c
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-09-03 08:56:59 +0200

    feat(logging): use lwma

--- src/utils/formatting.py
+++ src/utils/formatting.py
@@ -33,29 +33,30 @@ def log(*data, locals: bool = False):
 
 class WandbLog:
     def __init__(self, ctx: Context, steps: int):
-        self.mean_loss = torch.zeros([], device=ctx.model.device,
-                                     dtype=torch.float16 if ctx.model.float16 else torch.float)
+        self.mean_loss = 0
         self.start_time = time.time()
         self.ctx = ctx
         self.idx = 0
         self.steps = steps
 
     def __call__(self, current_loss: torch.Tensor, learning_rate: float):
-        self.idx += 1
-        self.mean_loss += current_loss
         curr_loss = current_loss.item() / self.ctx.log.loss_steps_per_print
-        rate = self.idx / (time.time() - self.start_time)
+        del current_loss
+
+        self.mean_loss = (self.mean_loss * self.idx + curr_loss * (self.idx + 1)) / (self.idx * 2 + 1)
+        self.idx += 1
+
+        rate = self.ctx.log.loss_steps_per_print * self.idx / (time.time() - self.start_time)
         tokens_per_day = 3600 * 24 * rate * self.ctx.model.batch_size * self.ctx.model.sequence_length
-        mean_loss = self.mean_loss.item() / self.idx
 
         pretty_print(f"[{self.idx:{len(str(self.steps))}d}/{self.steps}]",
                      f"Loss: {curr_loss:7.4f} -",
-                     f"Mean: {mean_loss:7.4f} |",
+                     f"Mean: {self.mean_loss:7.4f} |",
                      f"LR: {learning_rate:.6f} |",
                      f"Batch/s: {rate:6.3f} -",
                      f"Tokens/day: {tokens_per_day:11,.0f}")
-        wandb.log({"Loss": current_loss,
-                   "Mean Loss": mean_loss,
+        wandb.log({"Loss": curr_loss,
+                   "Mean Loss": self.mean_loss,
                    "Learning Rate": learning_rate,
-                   "Batches/sec": rate,
-                   "Tokens/Day": tokens_per_day})
+                   "Batches per Second": rate,
+                   "Tokens per Day": tokens_per_day})
