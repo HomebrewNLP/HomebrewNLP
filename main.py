57819bfa2fbab2161a1faa3965cb9cb4c651c38b
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-09-04 01:59:53 +0200

    feat(inference): add small cli

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argh
 import yaml
 
 from src.dataclass import Context
+from src.executable.inference import inference_cli
 from src.executable.preprocess import preprocess_data
 from src.executable.train import train_model
 from src.utils.formatting import syntax_print
@@ -49,11 +50,10 @@ def train(config_path: typing.Optional[str] = None, steps: int = 0, load_model: 
     train_model(ctx, steps, load_model)
 
 
-@argh.arg('prompt', help='Input text to the model')
 @argh.arg('-g', '--generated_tokens', default='20', help='Number of tokens to be generated after prompt')
 @argh.arg('-t', '--temp', default='0.7', help='Temperature of the model.\nlower = consistency\nhigher = "creativity"')
 @argh.arg('-c', '--config_path', help='Path for the config file')
-def inference(prompt: str, generated_tokens: int = 20, temp: float = 0.7, config_path: str = None):
+def inference(generated_tokens: int = 20, temp: float = 0.2, config_path: str = None):
     '''
     Runs inference of input data on desired model
     '''
@@ -61,10 +61,7 @@ def inference(prompt: str, generated_tokens: int = 20, temp: float = 0.7, config
 
     ctx = get_context(config_path)
 
-    # TODO: Load model (pretrained)
-    # complete(ctx, model, prompt, temp, generated_tokens)
-
-    raise NotImplementedError
+    inference_cli(ctx, float(temp), int(generated_tokens))
 
 
 if __name__ == '__main__':
--- src/executable/inference.py
+++ src/executable/inference.py
@@ -1,15 +1,30 @@
 import torch
 
 from src.dataclass import Context
-from src.utils.setup import encode, decode
+from src.model import LinearAttention
+from src.utils.setup import encode, decode, get_model
 
 
-def complete(ctx: Context, model: torch.nn.Module, prompt: str, temperature: float, generated_tokens: int) -> str:
-    out = inp = encode(prompt)
-
+def complete(ctx: Context, model: LinearAttention, prompt: str, temperature: float, generated_tokens: int) -> str:
+    out = inp = encode(prompt).to(ctx.model.device).view(1, -1)
     for i in range(len(prompt), len(prompt) + generated_tokens):
-        new_item = torch.distributions.one_hot_categorical.OneHotCategorical(model(inp)[i] / temperature).sample()
-        out = torch.cat([out, new_item], -1)
-        inp = new_item if ctx.eval.cache else out
-    # TODO: Reset cache
-    return decode(inp)
+        tmp = model(inp)[:, :, -1]
+        tmp += torch.rand_like(tmp).clamp(min=1e-9).log().neg().log() * (-temperature)
+        new_item = torch.argmax(tmp, -1).view(1, -1)
+        out = inp = torch.cat([out, new_item], -1)
+        if ctx.eval.cache:
+            inp = new_item
+    model.reset_cache()
+    return decode(out)
+
+
+@torch.no_grad()
+def inference_cli(ctx: Context, temperature: float, generated_tokens: int):
+    mod = get_model(ctx, False)
+    mod.eval()
+    while True:
+        try:
+            prompt = input("Prompt: ")
+        except KeyboardInterrupt:
+            break
+        print(complete(ctx, mod, prompt, temperature, generated_tokens))
