6faa5c3c0afa141ea914b8431318ede96241d230
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-09-03 05:11:30 +0200

    feat(train): add model loading (to continue training)

--- src/executable/train.py
+++ src/executable/train.py
@@ -1,5 +1,4 @@
 import time
-import typing
 
 import torch
 from deepspeed.runtime import lr_schedules
@@ -8,8 +7,6 @@ from src.dataclass import Context
 from src.dataset import get_dataset
 from src.utils.formatting import pretty_print
 from src.utils.setup import get_model
-from src.model import LinearAttentionCell, ParameterStore
-
 
 
 def clip_gradient(ctx: Context, mod: torch.nn.Module):
@@ -23,8 +20,10 @@ def clip_gradient(ctx: Context, mod: torch.nn.Module):
         p.grad.data.copy_(p.grad * grad_scale)
 
 
-def train_model(ctx: Context, steps=None):
+def train_model(ctx: Context, steps=None, load_model: bool = False):
     mod = get_model(ctx)
+    if load_model:
+        mod.load()
     if not ctx.model.offloading:
         mod = mod.apply(ctx.model.device)
     opt = torch.optim.AdamW(mod.parameters())
@@ -76,5 +75,5 @@ def train_model(ctx: Context, steps=None):
                 curr_loss = 0
             if ctx.model.steps_per_checkpoint and i % ctx.model.steps_per_checkpoint == 0:
                 mod.save()
-        if steps is not None and i > steps:
+        if steps and i > steps:
             return
