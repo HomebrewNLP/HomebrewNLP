945abc207c1df9e50375a7af51f0d68edd58f104
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-09-04 03:27:32 +0200

    feat(inference): add complete_batch

--- src/executable/inference.py
+++ src/executable/inference.py
@@ -1,3 +1,5 @@
+import typing
+
 import torch
 
 from src.dataclass import Context
@@ -5,17 +7,23 @@ from src.model import LinearAttention
 from src.utils.setup import encode, decode, get_model
 
 
-def complete(ctx: Context, model: LinearAttention, prompt: str, temperature: float, generated_tokens: int) -> str:
-    out = inp = encode(prompt).to(ctx.model.device).view(1, -1)
-    for i in range(len(prompt), len(prompt) + generated_tokens):
-        tmp = model(inp)[:, :, -1]
+def complete_batch(ctx: Context, model: LinearAttention, prompt: torch.Tensor, temperature: float,
+                   generated_tokens: int) -> typing.List[str]:
+    batch, prompt_size = prompt.size()
+    out = prompt
+    for i in range(prompt_size, prompt_size + generated_tokens):
+        tmp = model(prompt)[:, :, -1]
         tmp += torch.rand_like(tmp).clamp(min=1e-9).log().neg().log() * (-temperature)
         new_item = torch.argmax(tmp, -1).view(1, -1)
-        out = inp = torch.cat([out, new_item], -1)
+        out = prompt = torch.cat([out, new_item], -1)
         if ctx.eval.cache:
-            inp = new_item
+            prompt = new_item
     model.reset_cache()
-    return decode(out)
+    return [decode(o) for o in out.unbind(0)]
+
+
+def complete(ctx: Context, model: LinearAttention, prompt: str, temperature: float, generated_tokens: int) -> str:
+    return complete_batch(ctx, model, encode(prompt).to(ctx.model.device).view(1, -1), temperature, generated_tokens)[0]
 
 
 @torch.no_grad()
