80dfd717f5801f2a57090833afb94bd3d035c7f1
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-09-04 02:02:23 +0200

    style(setup): add load + offload to get_model

--- src/utils/setup.py
+++ src/utils/setup.py
@@ -2,8 +2,8 @@ import math
 import random
 
 import numpy as np
-
 import torch
+
 from src.dataclass import Context
 from src.model import LinearAttention
 from src.utils.formatting import print
@@ -35,53 +35,7 @@ def setup_torch(seed: int):
     torch.manual_seed(seed)
 
 
-def get_deepspeed_config(ctx: Context) -> dict:
-    return {"train_batch_size": ctx.model.batch_size * ctx.optimizer.gradient_accumulation_steps,
-            "gradient_accumulation_steps": ctx.optimizer.gradient_accumulation_steps,
-            "optimizer": {"type": ctx.optimizer.type,
-                          "params": {"betas": [0.9, ctx.optimizer.beta2],
-                                     "eps": ctx.optimizer.epsilon,
-                                     "weight_decay": ctx.optimizer.weight_decay
-                                     }
-                          },
-            "fp16": {"enabled": ctx.model.float16},
-            "zero_optimization": {"stage": 3,
-                                  "cpu_offload": ctx.optimizer.zero.cpu_offload,
-                                  "contiguous_gradients": ctx.optimizer.zero.contiguous_gradients,
-                                  "overlap_comm": ctx.optimizer.zero.overlap_comm,
-                                  "offload_param": {"device": ctx.optimizer.zero.offload_param.device,
-                                                    "pin_memory": ctx.optimizer.zero.offload_param.pin_memory},
-                                  "offload_optimizer": {"device": ctx.optimizer.zero.offload_optimizer.device,
-                                                        "pin_memory": ctx.optimizer.zero.offload_optimizer.pin_memory},
-                                  "stage3_max_live_parameters": ctx.optimizer.zero.stage3_max_live_parameters,
-                                  "stage3_max_reuse_distance": ctx.optimizer.zero.stage3_max_reuse_distance,
-                                  "stage3_prefetch_bucket_size": ctx.optimizer.zero.stage3_prefetch_bucket_size,
-                                  "stage3_param_persistence_threshold": ctx.optimizer.zero.stage3_param_persistence_threshold,
-                                  },
-            "activation_checkpointing": {"cpu_checkpointing": True, "contiguous_memory_optimization": True},
-            "steps_per_print": ctx.log.deepspeed_steps_per_print,
-            "wall_clock_breakdown": ctx.log.wall_clock_breakdown,
-            "dump_state": ctx.log.dump_state,
-            "scheduler": {"type": "OneCycle",
-                          "params": {"cycle_min_lr": ctx.optimizer.one_cycle.cycle_min_lr,
-                                     "cycle_max_lr": ctx.optimizer.one_cycle.cycle_max_lr,
-                                     "decay_lr_rate": ctx.optimizer.one_cycle.decay_lr_rate,
-                                     "cycle_first_step_size": ctx.optimizer.one_cycle.cycle_first_step_size,
-                                     "cycle_second_step_size": ctx.optimizer.one_cycle.cycle_second_step_size,
-                                     "cycle_first_stair_count": ctx.optimizer.one_cycle.cycle_first_stair_count,
-                                     "cycle_second_stair_count": ctx.optimizer.one_cycle.cycle_second_stair_count,
-                                     "decay_step_size": ctx.optimizer.one_cycle.decay_step_size,
-                                     "cycle_momentum": ctx.optimizer.one_cycle.cycle_momentum,
-                                     "cycle_min_mom": ctx.optimizer.one_cycle.cycle_min_mom,
-                                     "cycle_max_mom": ctx.optimizer.one_cycle.cycle_max_mom,
-                                     "decay_mom_rate": ctx.optimizer.one_cycle.decay_mom_rate,
-                                     "last_batch_iteration": ctx.optimizer.one_cycle.last_batch_iteration
-                                     }
-                          }
-            }
-
-
-def get_model(ctx: Context) -> LinearAttention:
+def get_model(ctx: Context, load_model: bool) -> LinearAttention:
     mod = LinearAttention(ctx).to(dtype=torch.float16 if ctx.model.float16 else torch.float)
 
     if ctx.model.print_on_init:
@@ -90,12 +44,16 @@ def get_model(ctx: Context) -> LinearAttention:
     parameters = sum(np.prod(p.size()) for p in filter(lambda p: p.requires_grad, mod.parameters()))
     base = int(math.log10(parameters) / 3)
     print(f'Parameters: {parameters / (1000 ** base):.1f}{" kMBT"[base]}')
+    if load_model:
+        mod.load()
+    if not ctx.model.offloading:
+        mod = mod.to(ctx.model.device)
     return mod
 
 
 def encode(prompt: str) -> torch.LongTensor:
-    return torch.as_tensor(np.frombuffer(prompt.encode('UTF-8'), np.uint8))
+    return torch.as_tensor(np.frombuffer(prompt.encode('UTF-8'), np.uint8)).long()
 
 
 def decode(output: torch.LongTensor) -> str:
-    return ''.join(chr(c) for c in output)
+    return ''.join(chr(c) for c in output.view(-1).unbind(0))
