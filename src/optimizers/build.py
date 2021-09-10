94f3840091a9a93a37d8d661fe9967ae3b71ece8
JackMcCoy <fdrdavegill@gmail.com>
2021-09-10 06:02:29 -0400

    fix(optim build) build params from ctx attrs

--- src/optimizers/build.py
+++ src/optimizers/build.py
@@ -15,15 +15,12 @@ def build_optimizer(ctx: Context, parameters: typing.Iterable[torch.nn.Parameter
     name = ctx.optimizer.type
     if name in OPTIMIZERS:
         return OPTIMIZERS[name](parameters, ctx.optimizer)
-    try:
-        optm = getattr(torch.optim, name)
-        if torch.optim.Optimizer not in inspect.getmro(optm):
-            raise ValueError("Optimizer must inherit from 'torch.optim.Optimizer'.")
-        params = {'params': parameters}
-        for key in inspect.signature(optm).parameters.keys():
-            if key in ctx.optimizer:
-                params[key] = getattr(ctx.optimizer, key)
-        return optm(**params)
-    except TypeError:
-        pretty_print(f'{name} is not a valid optimizer type.')
-        traceback.print_exc()
+    optm = getattr(torch.optim, name)
+    if torch.optim.Optimizer not in inspect.getmro(optm):
+        raise ValueError("Optimizer must inherit from 'torch.optim.Optimizer'.")
+    params = {'params': parameters}
+    for key in inspect.signature(optm).parameters.keys():
+        if hasattr(ctx.optimizer,key):
+            params[key] = getattr(ctx.optimizer, key)
+    optm = optm(**params)
+    return optm
